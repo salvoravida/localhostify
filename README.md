1036535bae5ecef1e9b35ccd8e499bde3dd26b47
Salvo Ravida <salvoravida@gmail.com>
2021-10-08 02:47:11 +0200

    add yargs - microbundle

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
+<p align="center">
+  <a href="https://www.npmjs.com/package/cors-tunnel"><img src="https://img.shields.io/npm/v/cors-tunnel.svg?style=flat-square"></a>
+  <a href="https://www.npmjs.com/package/cors-tunnel"><img src="https://img.shields.io/npm/dt/cors-tunnel.svg?style=flat-square"></a>
+  <img src="./cov-badge.svg"></a><br/>
+  CORS Tunnel - Because real-backends matters!!
+</p>
+
 # cors-tunnel
-Http reverse proxy with CORS and cookie tunneling - localhost:3001 -> api.prod.domain.com
+Http reverse proxy with CORS and cookie tunneling - localhost:3001 -> api.prod.domain.com.
+
+```
+locahost:3000 -> CORS-cookie-free tunnel-server localhost:3001 <- api.production.domain.com
+```
+## Usage
+If you need a cookie for auth the session, copy it from the Browser and save to a file ".cookie[.env]"
+```
+npx cors-tunnel -p 3001 -t https://api.prod.domain.com -c .cookie.prod"
+```
+
+Now your local Api server `http://localhost:3001` will forward requests to `https://api.prod.domain.com`
+adding CORS headers to the response.
+
+## Enojy dev/debug with real backend!
+Enjoy running your local frontend `http://localhost:3000` using real backend services!
+
+
+
+Contributors
+------------
+See [Contributors](https://github.com/salvoravida/redux-first-history/graphs/contributors).
+
+License
+-------
+[MIT License](https://github.com/salvoravida/redux-first-history/blob/master/LICENSE.md).
+
---
+++ src/index.js
@@ -0,0 +1,70 @@
+import * as httpProxy from "http-proxy";
+import yargs from "yargs";
+const fs = require("fs");
+const http = require("http");
+
+const version = require("../package.json").version;
+
+export function run() {
+  console.log("\nCORS Tunnel " + version);
+  const { argv } =  yargs(process.argv.slice(2))
+  const port = Number(argv.p || 3001);
+
+  if (!argv.t) throw new Error('No target!');
+
+  const target = argv.t;
+  const sessionCookie =argv.c ? fs.readFileSync(`${argv.c}`, "utf8").trim() : '';
+
+  const proxy = httpProxy.createProxyServer({});
+
+  /* override Cookie, Origin and Refer to target */
+  proxy.on("proxyReq", (proxyReq, req) => {
+    if (sessionCookie) proxyReq.setHeader("Cookie", sessionCookie);
+    proxyReq.setHeader("origin", target);
+
+    const referer = req.headers.referer;
+    const origin = req.headers.origin;
+    if (referer && origin) {
+      proxyReq.setHeader("referer", referer.replace(origin, target));
+    }
+  });
+
+  /* override headers from target to request */
+  proxy.on("proxyRes", (proxyRes, req) => {
+    proxyRes.headers["Access-Control-Allow-Headers"] = "content-type";
+    proxyRes.headers["Access-Control-Allow-Methods"] =
+      "GET, PUT, POST, DELETE, HEAD";
+    const origin = req.headers.origin;
+    if (origin) {
+      proxyRes.headers["Access-Control-Allow-Origin"] = origin;
+      proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
+    }
+  });
+
+  const server = http.createServer((req, res) => {
+    if (req.method !== "OPTIONS") {
+      console.log(req.method, req.url);
+      proxy.web(req, res, {
+        changeOrigin: true,
+        target,
+      });
+    } else {
+      /* set Options headers for cors fetch option*/
+      res.setHeader("Access-Control-Allow-Headers", "content-type");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, PUT, POST, DELETE, HEAD"
+      );
+      const origin = req.headers.origin;
+      if (origin) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+      }
+      res.end();
+    }
+  });
+
+  console.log(`http://localhost:${port} -> ${target}`);
+
+  server.listen(port);
+}
